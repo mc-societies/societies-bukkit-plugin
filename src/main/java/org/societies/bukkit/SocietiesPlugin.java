c58ece8e1b752bc8e695d154f2fc1ecf3c408ddb
p000ison <maximilian.ammann@googlemail.com>
2014-12-19 13:26:00 +0100

    Disabled libraries loading

--- src/main/java/org/societies/bukkit/SocietiesPlugin.java
+++ src/main/java/org/societies/bukkit/SocietiesPlugin.java
@@ -3,12 +3,7 @@ package org.societies.bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.societies.util.DependencyLoader;
 
-import java.io.File;
-import java.io.IOException;
-import java.lang.reflect.InvocationTargetException;
-import java.net.URL;
 import java.util.List;
 
 /**
@@ -20,32 +15,6 @@ public class SocietiesPlugin extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getLogger().info("Loading libraries...");
-        File destination = new File(getDataFolder(), "libraries");
-
-        try {
-
-            String libraries = System.getProperty("societies-libraries-url");
-
-            if (libraries == null) {
-                libraries = "http://dev.bukkit.org/media/files/836/988/societies-bukkit-plugin-libraries.jar";
-            }
-
-            new DependencyLoader(getLogger()).loadDependencies(
-                    new URL(libraries),
-                    destination
-            );
-        } catch (InvocationTargetException e) {
-            e.printStackTrace();
-            return;
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-            return;
-        } catch (IOException e) {
-            e.printStackTrace();
-            return;
-        }
-
         loader = new SocietiesLoader(getClassLoader(), this);
         loader.onEnable();
     }
